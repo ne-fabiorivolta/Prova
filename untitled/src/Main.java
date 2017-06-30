4fcf5d585271f463e2445bb475fe73260d57a744
nextre0029 <valentina.primiceri@nextre.it>
2017-06-30 15:40:15 +0200

    commit get pesniero

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,6 +4,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        Get_pensiero getPensiero1= new Get_pensiero();
+        Get_pensiero getPensiero2= new Get_pensiero();
+        getPensiero1.getPensiero(8,2);
+        getPensiero2.getPensiero(5,7);
+
 
         System.out.println("questo è il carciofo principale");
     }
