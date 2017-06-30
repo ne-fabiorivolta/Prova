d9c5c2617da44efec4460fe02f86705a3925fb3d
Agatino <agatino.dagate@nextre.it>
2017-06-30 15:04:04 +0200

    modificato carciofino principale_aga

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,8 +3,6 @@
  */
 public class Main {
     public static void main(String[] args) {
-
-
-        System.out.println("questo è il carciofo principale");
+        System.out.println("questo è il carciofo principale con una modifica da carciofino_di_agatino");
     }
 }
