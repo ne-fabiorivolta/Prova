4808376fdc0f76ff174f8102700361f29720a378
Fabio Rivolta <fabio.rivolta@nextre.it>
2017-07-03 11:34:53 +0200

    sistemato getpensiero

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -11,22 +11,9 @@ public class Main implements Get_pensiero {
     public static void main(String[] args) {
 
         Agatino aga = Agatino.creaaga();
+        aga.getPensiero();
 
-        Random r = new Random();
 
-        int pensiero1 = 0;
-        int pensiero2 = 0;
-        int i = 0;
-        while (pensiero1 < 6 && pensiero2 < 8) {
-            pensiero1 = r.nextInt(6);
-            pensiero2 = r.nextInt(8);
-            i++;
-            aga.getPensiero(pensiero1, pensiero2);
-            if (pensiero1 == 5 && pensiero2 == 7) {
-                System.out.println("get pensiero: " + pensiero1 + "." + pensiero2 + " numero di iterazioni: " + i);
-                break;
-            }
-            System.out.println("getPensiero: " + pensiero1 + "." + pensiero2 + " numero di iterazioni: " + i);
-        }
+
     }
 }
--- untitled/src/entity/Get_pensiero.java
+++ untitled/src/entity/Get_pensiero.java
@@ -1,15 +1,27 @@
 package entity;
 
+import java.util.Random;
+
 /**
  * Created by nextre0029 on 30/06/2017.
  */
 public interface Get_pensiero {
 
-   default void getPensiero(int pensiero1, int pensiero2) {
-       if (pensiero1==5 && pensiero2==7) {
-           System.out.println("Get Pensiero!");
-       } else {
-           System.out.println(" Sei una merdina ");
-       }
-   }
+    default void getPensiero() {
+        int pensiero1=0;
+        int pensiero2=0;
+        Random r = new Random();
+        int i = 0;
+        while (pensiero1 < 6 && pensiero2 < 8) {
+            pensiero1 = r.nextInt(6);
+            pensiero2 = r.nextInt(8);
+            i++;
+
+            if (pensiero1 == 5 && pensiero2 == 7) {
+                System.out.println("get pensiero: " + pensiero1 + "." + pensiero2 + " numero di iterazioni: " + i);
+                break;
+            }
+            System.out.println("getPensiero: " + pensiero1 + "." + pensiero2 + " numero di iterazioni: " + i);
+        }
+    }
 }
