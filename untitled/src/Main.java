4347c9d56d1f6f4f8f343121603341ed9b1c070e
Agatino <agatino.dagate@nextre.it>
2017-07-03 11:28:38 +0200

    modificato

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,30 +1,32 @@
+import entity.Agatino;
+import entity.Get_pensiero;
+import entity.Valentina;
+
 import java.util.Random;
 
 /**
  * Created by Rivolta Fabio on 30/06/2017.
  */
-public class Main {
+public class Main implements Get_pensiero {
     public static void main(String[] args) {
-        Get_pensiero getPensiero1 = new Get_pensiero();
+
+        Agatino aga = Agatino.creaaga();
 
         Random r = new Random();
 
         int pensiero1 = 0;
         int pensiero2 = 0;
-        int i=0;
+        int i = 0;
         while (pensiero1 < 6 && pensiero2 < 8) {
             pensiero1 = r.nextInt(6);
             pensiero2 = r.nextInt(8);
             i++;
-            getPensiero1.getPensiero(pensiero1, pensiero2);
+            aga.getPensiero(pensiero1, pensiero2);
             if (pensiero1 == 5 && pensiero2 == 7) {
-                System.out.println("get pensiero: " + pensiero1 + "." + pensiero2 +" numero di iterazioni: "+ i);
+                System.out.println("get pensiero: " + pensiero1 + "." + pensiero2 + " numero di iterazioni: " + i);
                 break;
             }
-            System.out.println("getPensiero: " + pensiero1 + "." + pensiero2 +" numero di iterazioni: "+ i);
+            System.out.println("getPensiero: " + pensiero1 + "." + pensiero2 + " numero di iterazioni: " + i);
         }
-
-
-        //System.out.println("questo è il carciofo principale");
     }
 }
